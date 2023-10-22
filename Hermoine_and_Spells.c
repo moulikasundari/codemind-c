f5a210ec13528e446781539bfa8941ecc60a96b9
moulikasundari <147791632+moulikasundari@users.noreply.github.com>
2023-10-22 12:13:44 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>c&&b>c)     {         printf("%d",a+b);     }     else  if(a>b &&c>b)     {         printf("%d",a+c);     }     else     {         printf("%d",b+c);     } }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>c&&b>c)
+    {
+        printf("%d",a+b);
+    }
+    else  if(a>b &&c>b)
+    {
+        printf("%d",a+c);
+    }
+    else
+    {
+        printf("%d",b+c);
+    }
+}
