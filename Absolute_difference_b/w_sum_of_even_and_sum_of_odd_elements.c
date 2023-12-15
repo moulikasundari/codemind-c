caaced746ebd627d9c4b237d9a24a59e630b8218
moulikasundari <147791632+moulikasundari@users.noreply.github.com>
2023-12-15 11:26:59 +0530

    #include<stdio.h> int main() {     int n,i,se=0,so=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {         se=se+arr[i];         }         else         {             so=so+arr[i];         }     }     if(se>so)     {         printf("%d",se-so);     }     else     {         printf("%d",so-se);     } }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,se=0,so=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+        se=se+arr[i];
+        }
+        else
+        {
+            so=so+arr[i];
+        }
+    }
+    if(se>so)
+    {
+        printf("%d",se-so);
+    }
+    else
+    {
+        printf("%d",so-se);
+    }
+}
