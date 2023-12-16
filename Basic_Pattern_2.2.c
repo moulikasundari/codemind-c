a19ab22a6ec51d2495925aa3dc9f2d76ca1ff60c
moulikasundari <147791632+moulikasundari@users.noreply.github.com>
2023-12-16 11:45:26 +0530

    #include<stdio.h> int main() {     int r;     scanf("%d",&r);     int i,j;     for(i=1;i<=r;i++)     {         for(j=1;j<=i;j++)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_2.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    scanf("%d",&r);
+    int i,j;
+    for(i=1;i<=r;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
