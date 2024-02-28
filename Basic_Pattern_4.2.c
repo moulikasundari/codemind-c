f58db229320ab624a0a623591679c614290ba640
moulikasundari <147791632+moulikasundari@users.noreply.github.com>
2024-02-28 19:40:45 +0530

    #include <stdio.h>
    int main() {
        int rows ; // Number of rows in the pattern
        scanf("%d",&rows);
        // Loop through each row
        int i,j;
        for (i = 0; i < rows; i++) {
            // Loop through each column
            for (j = 0; j < rows; j++) {
                if (( i% 2 == 0)) { // For even rows and even columns, or odd rows and odd columns, print 0
                    printf("1 ");
                } else { // For other cases, print 1
                    printf("0 ");
                }
            }
            printf("
    "); // Move to the next line after printing each row
        }
    
        return 0;
    }

---
+++ Basic_Pattern_4.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int rows ; // Number of rows in the pattern
+    scanf("%d",&rows);
+    // Loop through each row
+    int i,j;
+    for (i = 0; i < rows; i++) {
+        // Loop through each column
+        for (j = 0; j < rows; j++) {
+            if (( i% 2 == 0)) { // For even rows and even columns, or odd rows and odd columns, print 0
+                printf("1 ");
+            } else { // For other cases, print 1
+                printf("0 ");
+            }
+        }
+        printf("
+"); // Move to the next line after printing each row
+    }
+
+    return 0;
+}
